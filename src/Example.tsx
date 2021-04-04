be85d007315f1cb07ac9c03391b375d585e05956
Dominik Dorfmeister <office@dorfmeister.cc>
2021-04-04 14:38:21 +0200

    actually call useRepoData in the component

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,22 +1,18 @@
 import * as React from 'react';
-import { useQuery } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { useRepoData } from './hooks'
 
 export function Example() {
-    const { isLoading, error, data, isFetching } = useQuery('repoData', () =>
-        fetch(
-            'https://api.github.com/repos/tannerlinsley/react-query'
-        ).then((res) => res.json())
-    );
+    const { isLoading, error, data, isFetching } = useRepoData()
 
-    if (isLoading) return <div>Loading...</div>;
+    if (isLoading) return <div>Loading...</div>
 
     if (error instanceof Error)
-        return <div>An error has occurred: {error.message}</div>;
+        return <div>An error has occurred: {error.message}</div>
 
     return (
         <div>
-            <h1>name: {data.name}</h1>
+            <h1>name: {data?.name}</h1>
             <div>{isFetching ? 'Updating...' : ''}</div>
             <ReactQueryDevtools initialIsOpen />
         </div>
