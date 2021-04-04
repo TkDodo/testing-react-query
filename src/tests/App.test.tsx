6cfe2c79f1220cc1c250a170a8c987182f31f3e8
Dominik Dorfmeister <office@dorfmeister.cc>
2021-04-04 14:08:05 +0200

    failure query component

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,28 @@
 import { waitFor } from '@testing-library/react'
+import { rest } from 'msw'
 import { renderWithClient } from './utils'
+import { server } from '../setupTests'
 import { Example } from '../Example'
 
-test('my second test', async () => {
+test('successful query component', async () => {
     const result = renderWithClient(<Example />)
 
     await waitFor(() => result.getByText(/name/))
 
     expect(result.getByText(/name/)).toHaveTextContent('mocked-react-query')
+})
+
+test('failure query component', async () => {
+
+    server.use(
+        rest.get('*', (req, res, ctx) => {
+            return res(ctx.status(500))
+        })
+    )
+
+    const result = renderWithClient(<Example />)
+
+    await waitFor(() => result.getByText(/error/))
+
+    expect(result.getByText(/error/)).toHaveTextContent('An error has occurred')
 })
