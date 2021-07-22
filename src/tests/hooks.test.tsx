c5aa4856248aaf89bf6a9db5049d9563b01883d3
Dominik Dorfmeister <office@dorfmeister.cc>
2021-07-22 11:32:30 +0200

    test isFetching and refetch combo

--- src/tests/hooks.test.tsx
+++ src/tests/hooks.test.tsx
@@ -1,6 +1,6 @@
 import { rest } from 'msw'
 import * as React from 'react'
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { server } from '../setupTests'
 import { createWrapper } from './utils'
 import { useRepoData } from '../hooks'
@@ -14,6 +14,41 @@ describe('query hook', () => {
         await waitFor(() => result.current.isSuccess)
 
         expect(result.current.data?.name).toBe('mocked-react-query')
+    })
+
+    test('isFetching for disabled queries', async () => {
+        const { result, waitFor } = renderHook(() => useRepoData(), {
+            wrapper: createWrapper()
+        })
+
+        expect(result.current).toMatchObject({
+            isLoading: true,
+            isFetching: true,
+            data: undefined,
+        });
+
+        await waitFor(() => result.current.isSuccess)
+
+        expect(result.current.data?.name).toBe('mocked-react-query')
+
+        act(() => {
+            result.current.refetch()
+        })
+
+        await waitFor(() => result.current.isFetching)
+
+        expect(result.current).toMatchObject({
+            isLoading: false,
+            isFetching: true,
+        })
+
+        await waitFor(() => !result.current.isFetching)
+
+        expect(result.current).toMatchObject({
+            isLoading: false,
+            isSuccess: true,
+            isFetching: false,
+        })
     })
 
     test('failure query hook', async () => {
