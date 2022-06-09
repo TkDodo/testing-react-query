19be2c81e9d9a9443c033187bcd435f4664a6f3f
Dominik Dorfmeister <office@dorfmeister.cc>
2022-06-09 21:50:13 +0200

    react-query v4

--- src/tests/hooks.test.tsx
+++ src/tests/hooks.test.tsx
@@ -1,54 +1,19 @@
 import { rest } from 'msw'
 import * as React from 'react'
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act, waitFor } from '@testing-library/react'
 import { server } from '../setupTests'
 import { createWrapper } from './utils'
 import { useRepoData } from '../hooks'
 
 describe('query hook', () => {
     test('successful query hook', async () => {
-        const { result, waitFor } = renderHook(() => useRepoData(), {
+        const { result } = renderHook(() => useRepoData(), {
             wrapper: createWrapper()
         })
 
-        await waitFor(() => result.current.isSuccess)
+        await waitFor(() => expect(result.current.isSuccess).toBe(true))
 
         expect(result.current.data?.name).toBe('mocked-react-query')
-    })
-
-    test('isFetching for disabled queries', async () => {
-        const { result, waitFor } = renderHook(() => useRepoData(), {
-            wrapper: createWrapper()
-        })
-
-        expect(result.current).toMatchObject({
-            isLoading: true,
-            isFetching: true,
-            data: undefined,
-        });
-
-        await waitFor(() => result.current.isSuccess)
-
-        expect(result.current.data?.name).toBe('mocked-react-query')
-
-        act(() => {
-            result.current.refetch()
-        })
-
-        await waitFor(() => result.current.isFetching)
-
-        expect(result.current).toMatchObject({
-            isLoading: false,
-            isFetching: true,
-        })
-
-        await waitFor(() => !result.current.isFetching)
-
-        expect(result.current).toMatchObject({
-            isLoading: false,
-            isSuccess: true,
-            isFetching: false,
-        })
     })
 
     test('failure query hook', async () => {
@@ -58,11 +23,11 @@ describe('query hook', () => {
             })
         )
 
-        const { result, waitFor } = renderHook(() => useRepoData(), {
+        const { result } = renderHook(() => useRepoData(), {
             wrapper: createWrapper()
         })
 
-        await waitFor(() => result.current.isError)
+        await waitFor(() => expect(result.current.isError).toBe(true))
 
         expect(result.current.error).toBeDefined()
     })
